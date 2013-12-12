37145ed6d3b4e1ee0b8e467856f267980a097a5b
alexstrom <c15alex.strom@usafa.edu>
2013-12-12 15:14:41 -0700

    Create robit.h

---
+++ robit.h
@@ -0,0 +1,27 @@
+/*
+ * robit.h
+ *
+ *  Created on: Nov 19, 2013
+ *      Author: C15Alex.Strom
+ */
+
+#ifndef ROBIT_H_
+#define ROBIT_H_
+
+void moveRightMotorF();
+void moveLeftMotorF();
+void moveRightMotorB();
+void moveLeftMotorB();
+
+void moveForward();
+void moveBack();
+
+void moveRight();
+void moveLeft();
+
+void initRobot();
+void initTimer();
+
+
+
+#endif /* ROBOT_H_ */
